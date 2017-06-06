11524d571e9c2694c5e59fe9a33cdbadef9186b3
kl10kva <fynaxsel@mail.ru>
2017-06-07 01:20:22 +0900

    Add models: SubscribeBlog, MarkPost; Edit Model UML class

--- blogs/admin.py
+++ blogs/admin.py
@@ -1,6 +1,9 @@
 from django.contrib import admin
-from blogs.models import User, Blog, Post
+from blogs.models import User, Blog, Post, SubscribeBlog, MarkPost
 
 admin.site.register(User)
 admin.site.register(Blog)
 admin.site.register(Post)
+admin.site.register(SubscribeBlog)
+admin.site.register(MarkPost)
+
--- blogs/models.py
+++ blogs/models.py
@@ -15,7 +15,7 @@ class User(models.Model):
 
 class Blog(models.Model):
     """Информация о блогах"""
-    user = models.ForeignKey(User, verbose_name = "Пользователь")
+    user = models.ForeignKey(User, verbose_name = "Пользователь", on_delete = models.CASCADE)
     title = models.CharField(max_length = 40, unique = True, verbose_name = "Наименование блога")
     description = models.TextField(verbose_name = "Описание блога")
     creation_date = models.DateField(auto_now_add = True)
@@ -26,11 +26,43 @@ class Blog(models.Model):
 
 class Post(models.Model):
     """Информация о постах"""
-    blog = models.ForeignKey(Blog, verbose_name = "Блог")
+    blog = models.ForeignKey(Blog, verbose_name = "Блог", on_delete = models.CASCADE)
     title = models.CharField(max_length = 40, unique = True, verbose_name = "Наименование поста")
     description = models.TextField(verbose_name = "Содержание поста")
     creation_date = models.DateField(auto_now_add = True)
 
     def __str__(self):
-        return "(%s). %s: %s" % (self.creation_date, self.blog_id, self.title)
+        return "(%s). %s: %s" % (self.creation_date, self.blog, self.title)
+
+
+class SubscribeBlog(models.Model):
+    """Информация о подписках пользователей на блоги"""
+    NOT_SIGNED = False
+    SIGNED = True
+    STATUS = (
+        (NOT_SIGNED, "Не подписан"),
+        (SIGNED, "Подписан")
+    )
+    user = models.ForeignKey(User, verbose_name = "Пользователь", on_delete = models.CASCADE)
+    blog = models.ForeignKey(Blog, verbose_name = "Блог", on_delete = models.CASCADE)
+    status = models.BooleanField(choices = STATUS, verbose_name = "Статус подписки")
+
+    def __str__(self):
+        return "%s: %s (%s)" % (self.user, self.blog, self.status)
+
+
+class MarkPost(models.Model):
+    """Информация о помеченных пользователем постах"""
+    NOT_READ = False
+    READ = True
+    STATUS = (
+        (NOT_READ, "Не прочитано"),
+        (READ, "Прочитано")
+    )
+    subscribe_blog = models.ForeignKey(SubscribeBlog, verbose_name = "Подписка", on_delete = models.CASCADE)
+    post = models.ForeignKey(Post, verbose_name = "Пост", on_delete = models.CASCADE)
+    status = models.BooleanField(choices = STATUS, verbose_name = "Статус")
+
+    def __str__(self):
+        return "%s: %s (%s)" % (self.subscribe_blog, self.post, self.status)
 
